a2f928817542ce17cd311df7d6c2352ec517f92e
Bambang T. Lasiman <bambang.tryono@aladinbank.id>
2022-12-19 19:31:19 +0700

    percobaan kedua

---
+++ Conversion.py
@@ -0,0 +1,16 @@
+
+input_dari_keyboard = input("Masukkan nilai: ")
+
+nilai = int(input_dari_keyboard)
+
+if nilai > 6:
+    print("C")
+elif nilai > 9:
+    print("A")
+elif nilai > 7:
+    print("C")
+elif nilai > 5:
+    print("D")
+else:
+    print(nilai > 4)
+    print("E")
---
+++ Test.py
@@ -0,0 +1,11 @@
+
+nama = "Fitra Achmad Ramadhan"
+umur = 17
+agama = "Islam"
+
+
+print("Nama saya awal adalah : ", nama)
+
+nama_baru = nama.replace("Ramadhan","Romdhoni")
+
+print("Nama saya setelah diubah adalah : ", nama_baru)
